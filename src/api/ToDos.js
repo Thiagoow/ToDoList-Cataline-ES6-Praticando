95d5dc494c74fba156c94964beff2d4136fc9def
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-05-03 15:20:10 -0300

    Finalizada a aula 1 :D

--- src/api/ToDos.js
+++ src/api/ToDos.js
@@ -1,8 +1,27 @@
 import axios from "../utils/axios";
 
 export default class ToDos {
-  async index() {
-    const response = await axios.get("/ToDos");
-    return response;
+  //Criando a requisição get, para exibir todos os ToDos:
+  async list() {
+    const { data } = await axios.get("/ToDos");
+    //Retorna os ToDos, presentes no db.json:
+    return data;
+  }
+
+  //Criando a requisição post, para adicionar um novo ToDo:
+  async store({ text, done }) {
+    const { data } = await axios.post("/ToDos", { text, done });
+    return data;
+  }
+
+  //Criando a requisição put, para atualizar os ToDos:
+  async update({ id, text, done }) {
+    const { data } = await axios.put(`/ToDos/${id}`, { text, done });
+    return data;
+  }
+
+  //Criando a requisição delete, para deletar os ToDos:
+  async delete({ id }) {
+    await axios.delete(`/ToDos/${id}`);
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,41 @@
 import ToDos from "./api/ToDos";
 
-async function exec() {
+//Executa a requisição list (get) para listar todos os ToDos:
+async function listar() {
   const todos = new ToDos();
 
-  const response = await todos.index();
+  const response = await todos.list();
   console.log(response);
 }
 
-exec();
+async function adicionar() {
+  const todos = new ToDos();
+
+  const response = await todos.store({ text: "Cortar o cabelo", done: false });
+  console.log(response);
+}
+
+async function atualizar() {
+  const todos = new ToDos();
+
+  const response = await todos.update({
+    done: true,
+    text: "Beber Água",
+    id: 4,
+  });
+  console.log(response);
+}
+
+async function deletar() {
+  const todos = new ToDos();
+
+  const response = await todos.delete({});
+  console.log(response);
+}
+
+//adicionar();
+//atualizar();
+//deletar();
+/*Listar tem que ser por último, pq aí ele mostra 
+com todas as alterações já feitas pelos outros métodos:*/
+listar();
