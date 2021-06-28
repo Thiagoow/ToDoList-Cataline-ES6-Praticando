1a91f50e2d60c202f280ee9daecaa4593a475657
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-06-28 16:43:41 -0300

    Só falta o deploy funcionar 🤩🤩😎🤩

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,4 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://api-jsonserver-todolist.herokuapp.com"
+  // Se estiver local, substitua por -> "http://localhost:3000"
 });
